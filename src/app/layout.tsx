ba65794a9f6c3c7bececc45562f433d6125b3d8c
Chong, Ian <ian.chong@hilti.com>
2024-09-01 22:37:25 +0800

    Setup: Add fonts.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { ReactNode } from "react";
 import "./index.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Ian Chong - Fullstack Developer",
@@ -17,7 +14,7 @@ type RootLayoutProps = {
 export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
