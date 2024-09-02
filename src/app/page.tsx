45da76b0372f4c186f177ad15e61deb642a0989e
Chong, Ian <ian.chong@hilti.com>
2024-09-03 00:23:51 +0800

    Feature: Add image section.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
+import HeroGurulab from "@/assets/images/hero-gurulab.png";
 import Button from "@/ui/components/button";
 import InlineLink from "@/ui/components/inline-link";
 import NavBar from "@/ui/sections/navbar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <NavBar />
+      {/* Cover */}
       <div className="relative">
         <div className="absolute -z-10 h-full w-full">
           <div className="absolute left-1/3 top-1/2 h-[700px] w-[700px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#D17E16]/20 blur-[200px]"></div>
@@ -40,6 +43,19 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {/* Hero */}
+      <div className="relative">
+        <Image src={HeroGurulab} alt="Gurulab" className="h-auto w-full" />
+        <div className="absolute bottom-0 px-60 pb-[210px] pt-80">
+          <div className="flex max-w-[700px] flex-col gap-10">
+            <h1 className="text-gradient text-6xl font-bold leading-tight tracking-tight">
+              Crafting an experience means the careful blend of feel and
+              function.
+            </h1>
+          </div>
+        </div>
+      </div>
+      {/* Projects */}
     </main>
   );
 }
