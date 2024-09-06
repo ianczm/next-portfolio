629f620fcc7bd1583f5a415acff8137d4b20f1ab
Chong, Ian <ian.chong@hilti.com>
2024-09-07 02:12:17 +0800

    Refactor: Remove redundant fragment.

--- src/app/(pages)/(home)/projects.tsx
+++ src/app/(pages)/(home)/projects.tsx
@@ -5,51 +5,48 @@ import { Heading1, Heading2 } from "@/ui/components/typography";
 
 export function ProjectsSection() {
   return (
-    <>
-      <div className="relative">
-        <div className="absolute -z-10 h-full w-full">
-          <div className="absolute left-1/3 top-1/2 h-[700px] w-[700px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#D17E16]/20 blur-[200px]"></div>
-          <div className="absolute left-2/3 top-1/2 h-[600px] w-[600px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#FB8842]/20 blur-[200px]"></div>
+    <div className="relative">
+      <div className="absolute -z-10 h-full w-full">
+        <div className="absolute left-1/3 top-1/2 h-[700px] w-[700px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#D17E16]/20 blur-[200px]"></div>
+        <div className="absolute left-2/3 top-1/2 h-[600px] w-[600px] -translate-x-1/2 -translate-y-1/2 rounded-full bg-[#FB8842]/20 blur-[200px]"></div>
+      </div>
+      <div className="flex flex-col gap-16 p-60">
+        {/* Header */}
+        <div className="flex max-w-[700px] flex-col gap-10">
+          <Heading2>Featured Projects</Heading2>
+          <Heading1>
+            I've worked on some pretty <span className="text-transparent">fun projects.</span>
+          </Heading1>
         </div>
-        <div className="flex flex-col gap-16 p-60">
-          {/* Header */}
-          <div className="flex max-w-[700px] flex-col gap-10">
-            <Heading2>Featured Projects</Heading2>
-            <Heading1>
-              I've worked on some pretty <span className="text-transparent">fun projects.</span>
-            </Heading1>
+        {/* Projects container */}
+        <div className="flex gap-10">
+          <div className="flex flex-col gap-10 pt-36">
+            <Card src={ShotdeskGurulab} title="Gurulab" className="h-[875px] w-[700px]">
+              Welcoming home page with a cheerful and bright design language for an EduTech startup.
+            </Card>
+            <Card
+              src={ShotdeskJourneyOfLife}
+              title="Journey of Life"
+              className="h-[875px] w-[700px]"
+            >
+              Multiplayer game for event participants to experience the thrill of financial
+              decision-making.
+            </Card>
           </div>
-          {/* Projects container */}
-          <div className="flex gap-10">
-            <div className="flex flex-col gap-10 pt-36">
-              <Card src={ShotdeskGurulab} title="Gurulab" className="h-[875px] w-[700px]">
-                Welcoming home page with a cheerful and bright design language for an EduTech
-                startup.
-              </Card>
-              <Card
-                src={ShotdeskJourneyOfLife}
-                title="Journey of Life"
-                className="h-[875px] w-[700px]"
-              >
-                Multiplayer game for event participants to experience the thrill of financial
-                decision-making.
-              </Card>
-            </div>
-            <div className="flex flex-col gap-10">
-              <Card
-                src={ShotdeskJourneyOfLife}
-                title="This Portfolio"
-                className="h-[875px] w-[700px]"
-              >
-                A milestone in establishing a personal brand and consolidating my skills.
-              </Card>
-              <Card src={ShotdeskGurulab} title="More Projects" className="h-[875px] w-[700px]">
-                View other exciting ideas that I've been working on.
-              </Card>
-            </div>
+          <div className="flex flex-col gap-10">
+            <Card
+              src={ShotdeskJourneyOfLife}
+              title="This Portfolio"
+              className="h-[875px] w-[700px]"
+            >
+              A milestone in establishing a personal brand and consolidating my skills.
+            </Card>
+            <Card src={ShotdeskGurulab} title="More Projects" className="h-[875px] w-[700px]">
+              View other exciting ideas that I've been working on.
+            </Card>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/app/(pages)/(home)/testimonials.tsx
+++ src/app/(pages)/(home)/testimonials.tsx
@@ -4,40 +4,38 @@ import { GradientText } from "@/ui/components/typography/gradient-text";
 
 export function TestimonialsSection() {
   return (
-    <>
-      <div className="bg-dark-400">
-        <div className="flex flex-col gap-32 p-60">
-          <div className="flex flex-col gap-10">
-            <Heading2>What Clients Said</Heading2>
-            <hr />
-          </div>
-          <div className="grid grid-cols-[1fr_2fr] gap-10">
-            <div>
-              <Heading3>GuruLab</Heading3>
-              <GradientText as={Paragraph2} className="font-bold">
-                Emily Neoh
-              </GradientText>
-              <Paragraph1>Curriculum Content Lead</Paragraph1>
-            </div>
-            <div className="flex flex-col gap-10">
-              <Quote>
-                I enjoyed working with Ian. He grasped the project's principles and requirements
-                quickly, delivering outcomes to a high standard. I also valued the enthusiasm and
-                excellent communication that Ian brought, and would recommend his web development
-                services with no hesitation.
-              </Quote>
-              <div className="flex gap-2">
-                <Chip>Freelance</Chip>
-                <Chip>Full business site</Chip>
-                <Chip>Landing pages</Chip>
-                <Chip>Layout and design</Chip>
-                <Chip>Dynamic reports</Chip>
-              </div>
-            </div>
-          </div>
+    <div className="bg-dark-400">
+      <div className="flex flex-col gap-32 p-60">
+        <div className="flex flex-col gap-10">
+          <Heading2>What Clients Said</Heading2>
           <hr />
         </div>
+        <div className="grid grid-cols-[1fr_2fr] gap-10">
+          <div>
+            <Heading3>GuruLab</Heading3>
+            <GradientText as={Paragraph2} className="font-bold">
+              Emily Neoh
+            </GradientText>
+            <Paragraph1>Curriculum Content Lead</Paragraph1>
+          </div>
+          <div className="flex flex-col gap-10">
+            <Quote>
+              I enjoyed working with Ian. He grasped the project's principles and requirements
+              quickly, delivering outcomes to a high standard. I also valued the enthusiasm and
+              excellent communication that Ian brought, and would recommend his web development
+              services with no hesitation.
+            </Quote>
+            <div className="flex gap-2">
+              <Chip>Freelance</Chip>
+              <Chip>Full business site</Chip>
+              <Chip>Landing pages</Chip>
+              <Chip>Layout and design</Chip>
+              <Chip>Dynamic reports</Chip>
+            </div>
+          </div>
+        </div>
+        <hr />
       </div>
-    </>
+    </div>
   );
 }
