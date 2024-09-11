1ba09afab40f23400dc6e6fb861fc7e2dce19377
Chong, Ian <ian.chong@hilti.com>
2024-09-11 22:30:49 +0800

    Feature: Use larger tap areas for clickables.

--- src/ui/sections/navbar.tsx
+++ src/ui/sections/navbar.tsx
@@ -1,17 +1,24 @@
 import Button from "../components/button";
+import InlineLink from "../components/inline-link";
 import IanczmLogo from "../components/logo";
 import NavLink from "../components/nav-link";
 
 export default function NavBar() {
   return (
     <nav className="absolute left-0 top-0 z-50 flex w-full items-center justify-between px-60 py-20">
-      <a href="/">
+      <InlineLink href="/" className="-m-4 p-4">
         <IanczmLogo />
-      </a>
+      </InlineLink>
       <div className="flex items-center gap-20">
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/about">About</NavLink>
-        <NavLink href="/projects">Projects</NavLink>
+        <NavLink href="/" className="-m-4 p-4">
+          Home
+        </NavLink>
+        <NavLink href="/about" className="-m-4 p-4">
+          About
+        </NavLink>
+        <NavLink href="/projects" className="-m-4 p-4">
+          Projects
+        </NavLink>
         <Button intent="primary">Contact</Button>
       </div>
     </nav>
