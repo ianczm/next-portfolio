ed1ba3753ba0df6e30097573d468d73f7524b6a8
Chong, Ian <ian.chong@hilti.com>
2024-09-11 19:22:20 +0800

    Feature: Add glassmorphism to button.

--- src/ui/components/button.tsx
+++ src/ui/components/button.tsx
@@ -1,26 +1,30 @@
 import { cva, VariantProps } from "class-variance-authority";
 import { ComponentProps } from "react";
 
-const buttonStyles = cva(["rounded-full", "border-2", "px-9", "py-6", "transition-all"], {
-  variants: {
-    intent: {
-      primary: [
-        "border-transparent",
-        "bg-primary-400/20 hover:bg-primary-400",
-        "text-primary-400 hover:text-dark-800",
-        "shadow-2xl shadow-primary-400/10 hover:shadow-primary-400/60",
-      ],
-      secondary: [
-        "border-primary-400/25 hover:border-primary-400",
-        "shadow-2xl shadow-primary-400/10 hover:shadow-primary-400/20",
-        "bg-transparent hover:bg-primary-400/10",
-      ],
+const buttonStyles = cva(
+  ["rounded-full", "border-2", "px-9", "py-6", "transition-all", "backdrop-blur-3xl"],
+  {
+    variants: {
+      intent: {
+        primary: [
+          "border-transparent",
+          "bg-primary-400/20 hover:bg-primary-400",
+          "text-primary-400 hover:text-dark-800",
+          "shadow-2xl shadow-primary-400/10 hover:shadow-primary-400/60",
+        ],
+        secondary: [
+          "border-primary-400/20 hover:border-primary-400",
+          "bg-primary-400/5 hover:bg-primary-400/20",
+          "text-primary-400 hover:text-light-100",
+          "shadow-2xl shadow-primary-400/10 hover:shadow-primary-400/30",
+        ],
+      },
+    },
+    defaultVariants: {
+      intent: "secondary",
     },
   },
-  defaultVariants: {
-    intent: "secondary",
-  },
-});
+);
 
 interface ButtonProps extends ComponentProps<"button">, VariantProps<typeof buttonStyles> {}
 
