ca6f2a6e8e2b3168a7658bb1d56d040470aba625
Chong, Ian <ian.chong@hilti.com>
2024-09-28 19:48:55 +0800

    Refactor: Add customisation parameters for ScrollSkew.

--- src/ui/components/interaction/scroll-skew.tsx
+++ src/ui/components/interaction/scroll-skew.tsx
@@ -5,12 +5,22 @@ import gsap, { Power3 } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ComponentProps, useRef } from "react";
 
-export default function ScrollSkew({ children, className }: ComponentProps<"div">) {
+interface ScrollSkewProps extends ComponentProps<"div"> {
+  maxStrength?: number;
+  targetVelocity?: number;
+}
+
+export default function ScrollSkew({
+  children,
+  className,
+  maxStrength = 1.5,
+  targetVelocity = 4000,
+}: ScrollSkewProps) {
   const ref = useRef<HTMLDivElement>(null);
 
   useGSAP(
     () => {
-      const transformToSkew = gsap.utils.mapRange(0, 4000, 0, 1.5);
+      const transformToSkew = gsap.utils.mapRange(0, targetVelocity, 0, maxStrength);
 
       const skewTo = (skew: number) => {
         gsap.to(ref.current, { skewY: skew, duration: 1, ease: Power3.easeOut });
